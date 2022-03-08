9d0394c685f469bff87c6561c3d74bf712f90231
firengxuan <568420804@qq.com>
2022-03-08 22:49:52 +0800

    add

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new ResultInterceptor());
   app.useGlobalFilters(new HttpExceptionFilter());
-  await app.listen(3000);
+  app.enableCors();
+  await app.listen(4000);
 }
 bootstrap();
